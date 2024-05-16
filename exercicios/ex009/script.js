e372da7e8fe35e0e5d02f564bcac1629f2a3a2de
João Alberto Redondo <169093784+joaoalbertoredondo@users.noreply.github.com>
2024-05-16 17:38:38 -0300

    novos desafios

---
+++ exercicios/ex009/script.js
@@ -0,0 +1,13 @@
+let contador = 0
+
+let res = document.querySelector('section#result')
+
+function contar() {
+    contador ++
+    res.innerHTML = `<p>O contador está com ${contador} cliques.</p>`
+}
+
+function zerar() {
+    contador = 0
+    res.innerHTML = null
+}
